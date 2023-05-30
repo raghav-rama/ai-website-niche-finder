152b97be9e1c90c408a026bb5222ea3dfff8cebd
raghav-rama <hackerer528@gmail.com>
2023-05-30 14:01:51 +0530

    refactor: add more logging

--- aboip1/routes/index.py
+++ aboip1/routes/index.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template
 from config import Config
 from aboip1.views.InputForm import InputForm
+from aboip1.views.logging import getLogger
 
 OPENAI_API_KEY = Config.OPENAI_API_KEY
 
 bp = Blueprint("index", __name__)
 
+logger = getLogger()
 
 @bp.route("/")
 def index():
@@ -17,4 +19,4 @@ def index():
 
 @bp.route("/hello")
 def hello_template():
-    return render_template("hello.html", name=OPENAI_API_KEY)
+    return render_template("hello.html", name="AI Website Niche Finder")
