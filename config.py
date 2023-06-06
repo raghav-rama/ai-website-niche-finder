d042b9f48caa259d98356219192ba335b4055518
raghav-rama <hackerer528@gmail.com>
2023-06-07 00:51:49 +0530

    feat: add variable for session

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from flask import session
 
 # Load environment variables from .env file
 load_dotenv()
@@ -7,3 +8,5 @@ load_dotenv()
 
 class Config:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
+    session = session
