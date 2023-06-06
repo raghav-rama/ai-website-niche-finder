1ddd7655d9559890a0ab9277c91aed3963764f0a
raghav-rama <hackerer528@gmail.com>
2023-06-07 00:56:01 +0530

    feat: update app config
     - set response header Access-Control-Allow-Credentials as true

--- aboip1/__init__.py
+++ aboip1/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_cors import CORS
+from flask_session import Session
 from aboip1.routes.upload_csv import bp as upload_csv_bp
 from aboip1.routes.index import bp as index_bp
 from aboip1.routes.can_continue import bp as can_continue_bp
@@ -10,12 +11,16 @@ from aboip1.routes.download_csv import bp as download_csv_bp
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ["APP_SECRET_KEY"]
+    app.config["SESSION_TYPE"] = "filesystem"
+    app.config["SESSION_COOKIE_SECURE"] = True
+    app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+    Session(app)
 
     app.register_blueprint(index_bp)
     app.register_blueprint(upload_csv_bp)
     app.register_blueprint(can_continue_bp)
     app.register_blueprint(download_csv_bp)
 
-    CORS(app)
+    CORS(app, supports_credentials=True)
 
     return app
