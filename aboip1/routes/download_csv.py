6f5358e619a148bb18ba085071edc1d342739d16
raghav-rama <hackerer528@gmail.com>
2023-06-07 01:23:42 +0530

    feat: get uuid v4 from browser cookie download file accordingly

--- aboip1/routes/download_csv.py
+++ aboip1/routes/download_csv.py
@@ -1,7 +1,7 @@
-from flask import send_file, Blueprint, jsonify
+from flask import send_file, Blueprint, jsonify, request
 from aboip1.views.logging import getLogger
-from aboip1.views.inputs import Inputs
 from aboip1.views.helper import getFilePath, list_files
+from config import Config
 
 logger = getLogger()
 
@@ -11,7 +11,12 @@ bp = Blueprint("download_csv", __name__)
 @bp.route("/download_csv", methods=["GET"])
 def download_csv():
     try:
-        file_path = getFilePath()
+        cookies = request.cookies
+        logger.debug(f"cookies: {cookies}")
+        file_identifier = cookies.get("file_identifier")
+        logger.debug(f"session in download_csv: {Config.session}")
+        # logger.debug(f"sesion['file_identifier']: {Config.session['file_identifier']}")
+        file_path = getFilePath(file_identifier)
         logger.debug(f"file_path: {file_path}")
         logger.debug(f"list_files: {list_files()}")
         if file_path is None:
@@ -26,7 +31,9 @@ def download_csv():
                 404,
             )
         else:
-            return send_file(file_path, download_name="output.csv", as_attachment=True, max_age=600)
+            return send_file(
+                file_path, download_name="output.csv", as_attachment=True, max_age=600
+            )
     except Exception as e:
         logger.exception("Exception: ")
         return jsonify({"error": f"{e}"}), 404
