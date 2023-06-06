074341b5088061780e34ed547b50fe6fc9e3de82
raghav-rama <hackerer528@gmail.com>
2023-06-07 01:05:26 +0530

    debug: add dummy route for testing

--- aboip1/routes/index.py
+++ aboip1/routes/index.py
@@ -1,13 +1,11 @@
-from flask import Blueprint, render_template
-from config import Config
+from flask import Blueprint, render_template, make_response
 from aboip1.views.InputForm import InputForm
 from aboip1.views.logging import getLogger
-
-OPENAI_API_KEY = Config.OPENAI_API_KEY
 
 bp = Blueprint("index", __name__)
 
 logger = getLogger()
+
 
 @bp.route("/")
 def index():
@@ -20,3 +18,10 @@ def index():
 @bp.route("/hello")
 def hello_template():
     return render_template("hello.html", name="AI Website Niche Finder")
+
+# test route, not a feature
+@bp.route("/set_cookie")
+def cookie_insertion():
+    response = make_response("Cookie inserted")
+    response.set_cookie("file_identifier", value="values")
+    return response
