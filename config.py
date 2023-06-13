13897281508c647588a4957ddc64eb4b3d52329e
raghav-rama <hackerer528@gmail.com>
2023-06-14 03:53:08 +0530

    feat: update config
     - add AWS secrets

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 from flask import session
 
@@ -9,4 +10,10 @@ load_dotenv()
 class Config:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
+    AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+    AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+    AWS_REGION = os.getenv("AWS_REGION")
+    FROM_EMAIL = os.getenv("FROM_EMAIL")
+    TO_EMAILS = json.loads(os.getenv("TO_EMAILS"))
+
     session = session
